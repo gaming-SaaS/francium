7ceacb2e2efee140d380d32b52ce4cc02e37aadb
Ali Heidari <ali-heidari@outlook.com>
2022-07-22 16:18:42 +0430

    Add postgres provider

---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+
+import { Module } from '@nestjs/common';
+import { postgresProviders } from './postgres.provider';
+
+@Module({
+    providers: [...postgresProviders],
+    exports: [...postgresProviders],
+})
+export class DatabaseModule { }
